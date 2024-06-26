821065f05235edbc241742d8e2792cdb55a4c453
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2024-06-26 20:30:57 +0530

    delete

--- index.js
+++ index.js
@@ -80,6 +80,18 @@ app.patch("/posts/:id", (req, res) => {
 });
 
 //CHALLENGE 5: DELETE a specific post by providing the post id.
+app.delete("/posts/:id", (req, res) => {
+  const index = posts.findIndex((p) => p.id === parseInt(req.params.id));
+  if (index === -1) return res.status(404).json({ message: "Post not found" });
+
+  posts.splice(index, 1);
+  res.json({ message: "Post deleted" });
+});
+
+app.listen(port, () => {
+  console.log(`API is running at http://localhost:${port}`);
+});
+
 
 app.listen(port, () => {
   console.log(`API is running at http://localhost:${port}`);
