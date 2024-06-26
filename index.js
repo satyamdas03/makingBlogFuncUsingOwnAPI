1990c1aee85531c9792faeae018384ec0f6698eb
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2024-06-26 20:28:31 +0530

    get post using specific id

--- index.js
+++ index.js
@@ -45,6 +45,11 @@ app.get("/posts", (req, res) => {
 })
 
 //CHALLENGE 2: GET a specific post by id
+app.get("/posts/:id", (req, res) => {
+  const post = posts.find((p) => p.id === parseInt(req.params.id));
+  if (!post) return res.status(404).json({ message: "Post not found" });
+  res.json(post);
+})
 
 //CHALLENGE 3: POST a new post
 
