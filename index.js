24300ca2f7793924008044015d5b857918f0c494
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2024-06-26 20:29:14 +0530

    post new post

--- index.js
+++ index.js
@@ -52,6 +52,19 @@ app.get("/posts/:id", (req, res) => {
 })
 
 //CHALLENGE 3: POST a new post
+app.post("/posts", (req, res) => {
+  const newId = lastId += 1;
+  const post = {
+    id: newId,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    date: new Date(),
+  };
+  lastId = newId;
+  posts.push(post);
+  res.status(201).json(post);
+});
 
 //CHALLENGE 4: PATCH a post when you just want to update one parameter
 
